2b101f746f27c7d93aa3faa2d083228a90453cfa
Dawood Muzammil <dawoodmuzammil@hotmail.com>
2019-07-11 16:28:34 +0300

    compressed two attack methods into one

--- RPG_Game/Battle.cs
+++ RPG_Game/Battle.cs
@@ -35,13 +35,15 @@ namespace RPG_Game
                 if (turn)
                 {
                     turn = !turn;
-                    AttackByUser(p1, p2);
+                    //AttackByUser(p1, p2);
+                    Attack(p1, p2);
                     
                 }
                 else
                 {
                     turn = !turn;
-                    AttackByCPU(p1, p2);
+                    //AttackByCPU(p1, p2);
+                    Attack(p2, p1);
                 }
                 fileManager.InsertPlayerJSON(p1);
                 fileManager.InsertPlayerJSON(p2);
@@ -49,74 +51,163 @@ namespace RPG_Game
             }
         }
 
-        public void AttackByUser( Player p1, Player p2)
+        public void Attack(Player attacker, Player defender)
         {
-            
-            int input = 0;
-            do
+            int attackChoice = 0;
+            int attackMove = 0;
+            int damage = 0;
+
+            if (attacker is PlayerCPU)
             {
-                // Display options 
-                Console.WriteLine("1. Attack");
-                Console.WriteLine("0. Forfeit Match");
+                attackMove = RandomNumberGenerator.GenerateRandomNumber(2);
+                damage = CalculateDamage(attacker, defender, attackMove);
+            }
+            else if (attacker is PlayerUser)
+            {
+                // ask if user wishes to attack or forfeit game
+                attackChoice = GetAttackChoice();
 
-                Console.Write("\nYour option: ");
-                input = Convert.ToInt32(Console.ReadLine()); // user enters choice 
-
-                // user decides to attack
-                if (input == 1)
+                // user entered an invalid input
+                while (attackChoice != 1 && attackChoice != 9)
                 {
-                    Console.WriteLine("\nWhich move would you like to hit your opponent with?");
+                    Console.WriteLine("\nInvalid selection. Please try again.\n");
+                    attackChoice = GetAttackChoice();
+                }
 
-                    int option = 0;
-                    // ask user for whih move he wishes to play
-                    do
+                // user selects to attack
+                if ( attackChoice == 1)
+                {
+                    // get input
+                    DisplayAttackMoves(attacker);
+                    attackMove = GetAttackMove();
+
+                    // ask again if input invalid
+                    while ( attackMove < 1 && attackMove > 2)
                     {
-                        // display skills
-                        Console.WriteLine("1. " + p1.Character.AttackSkills[0]);
-                        Console.WriteLine("2. " + p1.Character.AttackSkills[1]);
-                        Console.Write("\nYour option: ");
-                        option = Convert.ToInt32(Console.ReadLine()) - 1; // ask for input
-
-                        if (option != 1 && option != 2)
-                            Console.WriteLine("Invalid choice. Please enter again...\n");
-
-                    } while (option == 1 && option == 2);
-
-                    // calculate effectiveness of attack
-                    int attackValue = p1.Character.Attack(option);
-                    int defenseValue = p2.Character.Defend(attackValue);
-                    int effectiveness = attackValue - defenseValue;
-
-                    Console.Clear();
-
-                    DisplayMoveReport(option, attackValue, defenseValue, effectiveness, p1, p2);
-
-                    p2.Character.HpValue -= effectiveness; // decrease CPU's HP
-
-                    PrintHPStatus(p1, p2); // print hp status
-
-                    if (p2.Character.IsLost(p2.Character.HpValue)) // checking if CPU's HP is less than 0
-                    {
-
-                        PrintRoundWinMessage(p1);
-                        Spawn(p1, p1.Level);
+                        Console.WriteLine("\nInvalid selection. Please try again.\n");
+                        DisplayAttackMoves(attacker);
+                        attackMove = GetAttackMove();
                     }
+                    // attack
+                    damage = CalculateDamage(attacker, defender, attackChoice);
                 }
-                else if (input == 0)
-                {
-                    Console.WriteLine("\n\n************************* G A M E  O V E R *************************\nThank you for playung Borda's RPG Game.\n");
-                    Environment.Exit(0);
-                }
-                else
-                {
-                    Console.WriteLine("\nInvalid choice. Please try again.\n");
-                }
-            } while (input != 1 && input != 0);
+            }
+
+            // this will be applied to all players
+            Console.Clear();
+
+            //DisplayMoveReport(option, attackValue, defenseValue, effectiveness, p1, p2);
+
+            defender.Character.HpValue -= damage; // decrease CPU's HP
+
+            PrintHPStatus(attacker, defender); // print hp status
         }
+
+        public int CalculateDamage( Player attacker, Player defender, int attackMove)
+        {
+            int attackValue = attacker.Character.Attack(attackMove);
+            int defenseValue = defender.Character.Defend(attackValue);
+
+            return (attackValue - defenseValue);
+        }
+
+
+        // NAME TO BE CHANGED
+        public int GetAttackChoice()
+        {
+            Console.WriteLine("1. Attack");
+            Console.WriteLine("0. Forfeit Match");
+
+            Console.Write("\nYour option: ");
+            int input = Convert.ToInt32(Console.ReadLine()); // user enters choice 
+
+            return input;
+        }
+
+        public void DisplayAttackMoves(Player attacker)
+        {
+            Console.WriteLine("\nWhich move would you like to hit your opponent with?");
+
+            Console.WriteLine("1. " + attacker.Character.AttackSkills[0]);
+            Console.WriteLine("2. " + attacker.Character.AttackSkills[1]);
+            Console.Write("\nYour option: ");
+        }
+
+        public int GetAttackMove()
+        {
+            int input = Convert.ToInt32(Console.ReadLine()); // user enters choice 
+
+            return input;
+        }
+
+        //public void AttackByUser( Player p1, Player p2)
+        //{
+            
+        //    int input = 0;
+        //    do
+        //    {
+        //        // Display options 
+        //        Console.WriteLine("1. Attack");
+        //        Console.WriteLine("0. Forfeit Match");
+
+        //        Console.Write("\nYour option: ");
+        //        input = Convert.ToInt32(Console.ReadLine()); // user enters choice 
+
+        //        // user decides to attack
+        //        if (input == 1)
+        //        {
+        //            Console.WriteLine("\nWhich move would you like to hit your opponent with?");
+
+        //            int option = 0;
+        //            // ask user for whih move he wishes to play
+        //            do
+        //            {
+        //                // display skills
+        //                Console.WriteLine("1. " + p1.Character.AttackSkills[0]);
+        //                Console.WriteLine("2. " + p1.Character.AttackSkills[1]);
+        //                Console.Write("\nYour option: ");
+        //                option = Convert.ToInt32(Console.ReadLine()) - 1; // ask for input
+
+        //                if (option != 1 && option != 2)
+        //                    Console.WriteLine("Invalid choice. Please enter again...\n");
+
+        //            } while (option == 1 && option == 2);
+
+        //            // calculate effectiveness of attack
+        //            int attackValue = p1.Character.Attack(option);
+        //            int defenseValue = p2.Character.Defend(attackValue);
+        //            int effectiveness = attackValue - defenseValue;
+
+        //            Console.Clear();
+
+        //            DisplayMoveReport(option, attackValue, defenseValue, effectiveness, p1, p2);
+
+        //            p2.Character.HpValue -= effectiveness; // decrease CPU's HP
+
+        //            PrintHPStatus(p1, p2); // print hp status
+
+        //            if (p2.Character.IsLost(p2.Character.HpValue)) // checking if CPU's HP is less than 0
+        //            {
+
+        //                PrintRoundWinMessage(p1);
+        //                Spawn(p1, p1.Level);
+        //            }
+        //        }
+        //        else if (input == 0)
+        //        {
+        //            Console.WriteLine("\n\n************************* G A M E  O V E R *************************\nThank you for playung Borda's RPG Game.\n");
+        //            Environment.Exit(0);
+        //        }
+        //        else
+        //        {
+        //            Console.WriteLine("\nInvalid choice. Please try again.\n");
+        //        }
+        //    } while (input != 1 && input != 0);
+        //}
 
         public void PrintRoundWinMessage(Player user)
         {
-            Console.WriteLine("\n\nCONGRATULATIONS. You have won this round. You have earned " + user.AwardPoints(p1.Character.HpValue) + " points.");
+            Console.WriteLine("\n\nCONGRATULATIONS. You have won this round. You have earned " + user.AwardPoints(user.Character.HpValue) + " points.");
             Console.WriteLine("You will now be put into the battlefield with a tougher opponent. Your HP is now set to " + user.Character.HpValue + "\n\n");
         }
 
@@ -132,26 +223,26 @@ namespace RPG_Game
         }
 
         // method where CPU attacks the player
-        public void AttackByCPU( Player p1, Player p2)
-        {        
-            int attackOption = RandomNumberGenerator.GenerateRandomNumber(2);
-            int attackValue = p2.Character.Attack( attackOption);
-            int defenseValue = p1.Character.Defend(attackValue);
-            int effectiveness = attackValue - defenseValue;
+        //public void AttackByCPU( Player p1, Player p2)
+        //{        
+        //    int attackOption = RandomNumberGenerator.GenerateRandomNumber(2);
+        //    int attackValue = p2.Character.Attack( attackOption);
+        //    int defenseValue = p1.Character.Defend(attackValue);
+        //    int effectiveness = attackValue - defenseValue;
 
-            DisplayMoveReport(attackOption, attackValue, defenseValue, effectiveness, p2, p1);
+        //    DisplayMoveReport(attackOption, attackValue, defenseValue, effectiveness, p2, p1);
 
-            p1.Character.HpValue -= effectiveness;
+        //    p1.Character.HpValue -= effectiveness;
 
-            PrintHPStatus( p1, p2);
+        //    PrintHPStatus( p1, p2);
 
-            // check if the user has lost... Game over if true
-            if (p1.Character.IsLost(p1.Character.HpValue)) // checking if CPU's HP is less than 0
-            {
-                Console.WriteLine("\n\nG   A   M   E      O   V   E   R\n\n"); // display message
-                fileManager.DeleteAllPlayers(); // clear JSON files
-            }
-        }
+        //    // check if the user has lost... Game over if true
+        //    if (p1.Character.IsLost(p1.Character.HpValue)) // checking if CPU's HP is less than 0
+        //    {
+        //        Console.WriteLine("\n\nG   A   M   E      O   V   E   R\n\n"); // display message
+        //        fileManager.DeleteAllPlayers(); // clear JSON files
+        //    }
+        //}
 
 
         public void Spawn(Player p1, int level)
